78753afc9eb35825cdb04fabd9ec19aa8f46fd08
ahrojoml <andres.huerta@mercadolibre.cl>
2024-01-04 10:49:49 -0300

    test: fix tests failling due to new error implementation

--- intro-func/salary/salary_tax_test.go
+++ intro-func/salary/salary_tax_test.go
@@ -2,6 +2,7 @@ package main_test
 
 import (
 	salary "go-bases/intro-func/salary"
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -11,25 +12,32 @@ func TestGetTax_Below50(t *testing.T) {
 	payment := 49999.0
 	expect := 0.0
 
-	result := salary.GetTax(payment)
+	result, err := salary.GetTax(payment)
 
 	require.Equal(t, expect, result)
+	require.NoError(t, err)
 }
 
 func TestGetTax_Over50Below150(t *testing.T) {
 	payment := 140000.0
-	expect := 14000.0
+	expect := payment * 0.1
 
-	result := salary.GetTax(payment)
+	result, err := salary.GetTax(payment)
 
 	require.Equal(t, expect, result)
+	require.NoError(t, err)
 }
 
 func TestGetTax_Over150(t *testing.T) {
 	payment := 170000.0
-	expect := 45900.0
+	var expect float64
+	expect += payment * 0.17
+	expect += payment * 0.1
 
-	result := salary.GetTax(payment)
+	result, err := salary.GetTax(payment)
 
-	require.Equal(t, expect, result)
+	diff := math.Abs(result - expect)
+
+	require.LessOrEqual(t, diff, 1e-9) // epsilon to compare floats
+	require.NoError(t, err)
 }
